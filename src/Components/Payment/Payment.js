4ddca59dca49bf525c1b796fcdb3c574875db21c
Farhan Eshrak <feshrakfreelancing01@gmail.com>
2021-04-18 17:51:40 +0600

    first commit

--- src/Components/Payment/Payment.js
+++ src/Components/Payment/Payment.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Elements, CardElement } from '@stripe/react-stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
-import PaymentForm from '../PaymentForm/PaymentForm';
-import SplitForm from '../PaymentForm/SplitForm';
+
 import SplitCardForm from '../PaymentForm/SplitCardForm';
 
 const stripePromise = loadStripe('pk_test_51IhHcNJWiTsGykf5SEHeGp8genuIXFfgR9NJDcMEX3VXHsXSBkDLjCjAn2jIt4hP9Du5NOpIZMo2Wh6hsWaPWqpG00BpL41BER');
